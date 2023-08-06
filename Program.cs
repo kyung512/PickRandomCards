1e6a090f48d4b70ebd145616367491592d948f77
naks1 <naks1@LAPTOP-ALF4K35C>
2023-08-06 05:35:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+
+            PracticeParse();
+        }
+
+        private static void PracticeParse()
+        {
+            int res;
+            string myStr = "120";
+            res = int.Parse(myStr);
+            Console.WriteLine("String is a numeric representation : " + res);
+
+            bool res1;
+            //int a;
+            res1 = int.TryParse(myStr, out int a);
+            Console.WriteLine("String is a numeric representation : " + res1);
+        }
+    }
+}
